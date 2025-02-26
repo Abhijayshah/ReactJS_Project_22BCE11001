eb523c88a0e102c852a18604553820f2f8cc96ae
Abhijayshah <abhijayshah74@gmail.com>
2025-02-27 03:19:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      <main style={{height:"80vh "}} >
+      <h1>HOME : My React App</h1>
+
+      </main>
+      
+      <Footer/>
+
+    </>
+
+  );
+}
+
+export default App;
